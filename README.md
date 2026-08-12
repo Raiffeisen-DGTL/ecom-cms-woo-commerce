c5b8ccec2b5367e85a91190c04b55bf536af3090
user-ecom-git <ecom@raiffeisen.ru>
2026-08-12 09:07:08 +0000

    Release f8311f60

---
+++ README.md
@@ -0,0 +1,12 @@
+# Райффайзен платежный плагин для Woocommerce
+
+Этот [Wordpress](https://wordpress.org/) плагин позволяет [Woocommerce](https://wordpress.org/plugins/woocommerce/) совершать платежи через Райффайзен Банк
+
+## Установка
+
+Плагин зависит от Woocommerce 3 и [PHP](https://php.net/) 7 с [cURL extension]
+
+### Ручная установка
+
+Скопируйте данную папку в папку с плагинами (wp-content/plugins/)
+
---
+++ includes/page-sync.php
@@ -0,0 +1,16 @@
+<div class="wrap" id="page-sync">
+	<h1>
+        Синхронизация счетов Райффайзен
+	</h1>
+	<p>
+		<button class="button button-primary">
+            Синхронизировать
+		</button>
+		<progress class="hidden">
+	</p>
+	<output>
+		<?php
+           echo 'Синхронизация счетов. Если у Вас не обновляются статусы оплаты заказов, то произведите обновление(синхронизацию с банком) вручную.';
+		?>
+	</output>
+</div>
